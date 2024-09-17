0c22644db2e22d65c79e3c01c02b396b78c7cf88
nikiforovall <alexey.nikiforovall@gmail.com>
2024-09-17 09:25:25 +0300

    fix ci

--- analyzer/Nall.NEST.MigtarionAnalyzer.CodeFixes/CodeFixResources.Designer.cs
+++ analyzer/Nall.NEST.MigtarionAnalyzer.CodeFixes/CodeFixResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Analyzer {
+namespace Nall.NEST.MigtarionAnalyzer {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Analyzer {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Analyzer.CodeFixResources", typeof(CodeFixResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Nall.NEST.MigtarionAnalyzer.CodeFixResources", typeof(CodeFixResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- analyzer/Nall.NEST.MigtarionAnalyzer/Resources.Designer.cs
+++ analyzer/Nall.NEST.MigtarionAnalyzer/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Analayzer {
+namespace Nall.NEST.MigtarionAnalyzer {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Analayzer {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Analayzer.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Nall.NEST.MigtarionAnalyzer.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
